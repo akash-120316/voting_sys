8f80364a5996e6aa6218b6aedb268f1f219f8206
akash-120216 <akash16.04.2005@gmail.com>
2024-10-13 17:59:22 +0530

    1

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+public class Main {
+    int global;
+
+    public void initialise(int a) {
+        global = a;
+    }
+
+    public void update(int a) {
+        global = a;
+    }
+
+    public int display() {
+        return global;
+    }
+
+    public void delete() {
+        global = 0;
+    }
+
+    public static void main(String[] args) {
+        Main ob = new Main();
+        Candidate_name os = new Candidate_name();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    int num = sc.nextInt();
+                    ob.initialise(num);
+                    break;
+                case 2:
+                    num = sc.nextInt();
+                    ob.initialise(num);
+                    break;
+                case 3:
+
+                    System.out.println(ob.display());
+                    break;
+                case 4:
+                    ob.delete();
+                    break;
+                case 5:
+                    sc.close();
+                    break;
+                default:
+                    System.out.println("Enter valid");
+                    break;
+            }
+        }
+
+
+    }
+}
